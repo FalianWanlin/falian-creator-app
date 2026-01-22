ab6cd4f5eabe7e0c5e679dca302f44bedd0ed024
Falian Wanlin <falian.wanlin@gmail.com>
2026-01-22 17:55:01 -0500

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div style={{ fontFamily: "system-ui", background: "#0e0e11", color: "#fff", minHeight: "100vh" }}>
+      <header style={{ padding: "16px 24px", borderBottom: "1px solid #222" }}>
+        <strong>Falian Creator</strong>
+      </header>
+
+      <main style={{ padding: "24px" }}>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
+}
