82cf42b52e068b744c216eb8aa88146914668959
Falian Wanlin <falian.wanlin@gmail.com>
2026-01-22 18:01:11 -0500

    Add System component for Falian Creator overview

---
+++ pages/system.js
@@ -0,0 +1,16 @@
+export default function System() {
+  return (
+    <>
+      <h1>Falian Creator System</h1>
+      <p>16-Zone Architecture Overview</p>
+
+      <ul>
+        <li>AI Support</li>
+        <li>Modular Structure</li>
+        <li>Role-based Interface</li>
+        <li>Multilingual Publishing</li>
+        <li>⋯</li>
+      </ul>
+    </>
+  );
+}
