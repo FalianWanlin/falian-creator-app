17720d197900103a21ae3563d6f0adf767ea0533
Falian Wanlin <falian.wanlin@gmail.com>
2026-01-22 18:00:08 -0500

    Add Home component with introductory content

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Falian Creator Workbench</h1>
+      <p>From the game, return to your real life.</p>
+
+      <div style={{ marginTop: 24 }}>
+        <a href="/workbench">Start Creating →</a>
+        <br /><br />
+        <a href="/system">Explore the System →</a>
+      </div>
+    </>
+  );
+}
